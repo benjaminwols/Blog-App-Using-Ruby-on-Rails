5831760440daa6079499571b4c0dbfff00c8dd74
Benjamin Wols <benjamin@ans.app>
2024-10-07 20:55:01 +0200

    Add seeds to setup the database with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+users = 5.times.map do
+  User.create(
+    email: "#{Faker::Name.first_name.downcase}@gmail.com",
+    password: "Welkom01!"
+  )
+end
+
+users.each do |user|
+  post = Post.create!(
+    title: Faker::Book.title,
+    body: Faker::Lorem.paragraphs(number:2).join("\n"),
+    status: Comment::VALID_STATUSES.sample
+  )
+
+  5.times do
+    post.comments.create(
+      author: Faker::Book.author,
+      body: Faker::Lorem.sentences(number: 2).join("\n"),
+      status: Comment::VALID_STATUSES.sample
+    )
+  end
+end
