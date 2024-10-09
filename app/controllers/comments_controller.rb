1d970779594a3a18434b7c5c6a604d36bd8de956
Max Pohlmann <maximilian.pohlmann98@gmail.com>
2024-10-09 21:45:08 +0200

    Implement comments exporter - route and controller - controller test incl. relevant fixtures - download button on homepage

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
+    require 'csv'
+
     def new
         @post = Post.find(params[:post_id])
         @comment = @post.comments.new
     end
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.new(comment_params)
@@ -12,8 +15,37 @@ class CommentsController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+
+    # Sends a CSV file containing an export of all visible comments of visible posts, ordered 
+    # ascendingly by the time of the comments' creation, with three columns: 
+    # "post title", "comment author", "comment body"
+    def export
+        current_datetime_string = DateTime.now.utc.strftime("%Y-%d-%m_%H-%M-%S")
+
+        send_data generate_export_csv,
+            filename: "all_comments_export_#{current_datetime_string}.csv",
+            type: "text/csv"
+    end
+
     private
-        def comment_params
-            params.require(:comment).permit(:author, :body, :status)
+
+    def comment_params
+        params.require(:comment).permit(:author, :body, :status)
+    end
+
+    def generate_export_csv
+        header_row = ["post title", "comment author", "comment body"]
+
+        data_rows = Comment
+            .joins(:post)
+            .where.not('comments.status': 'archived')
+            .where.not('posts.status': 'archived')
+            .order('comments.created_at': :asc)
+            .pluck(:'posts.title', :'comments.author', :'comments.body')
+
+        CSV.generate do |csv|
+            csv << header_row
+            data_rows.each { |row| csv << row }
         end
+    end
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
+require "csv"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "export with visible comments" do
+    get comments_export_path
+    assert_response :success
+    assert_equal("text/csv", response.header["Content-Type"])
+
+    rows = CSV.parse(response.body, headers: true)
+    assert_equal(rows.headers, ["post title", "comment author", "comment body"])
+
+    # Make sure that only the visible comments of visible posts are returned
+    assert_equal(rows.length, 2)
+
+    [comments(:one), comments(:two)].each do |expected_comment|
+      assert(rows.any? do |row| 
+        row["post title"] == expected_comment.post.title and
+        row["comment author"] == expected_comment.author and
+        row["comment body"] == expected_comment.body
+      end)
+    end
+  end
+
+  test "export without visible comments" do
+    comments(:one).delete()
+    comments(:two).delete()
+
+    get comments_export_path
+
+    rows = CSV.parse(response.body, headers: true)
+    assert_equal(rows.headers, ["post title", "comment author", "comment body"])
+
+    assert_equal(rows.length, 0)
+  end
 end
