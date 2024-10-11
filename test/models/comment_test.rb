7e4dec492e24d4ccd7f971a0b8200f8e6779455e
Benjamin Wols <benjamin@ans.app>
2024-10-11 08:55:51 +0200

    Add test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "to csv" do
+    assert_equal(
+      "Post title,Comment author,Comment body\nTitle1,Author,Body\n",
+      posts(:one).comments.to_csv
+    )
+  end
 end
